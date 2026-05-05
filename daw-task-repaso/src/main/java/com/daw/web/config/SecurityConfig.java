eec5606dffb0062df83cace329ce34b0509ea1a9
vricheb973 <vricheb973@g.educaand.es>
2026-05-05 09:07:38 +0200

    Subido JWT funcionando

---
+++ daw-task-repaso/src/main/java/com/daw/web/config/SecurityConfig.java
@@ -0,0 +1,81 @@
+package com.daw.web.config;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class SecurityConfig {
+	
+	@Value("${frontend.url}")
+	private String frontendUrls;
+	
+	@Autowired
+	private JwtFilter jwtFilter;	
+	
+	@Bean
+	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http
+			.csrf(csrf -> csrf.disable())
+			.cors(cors -> cors.configurationSource(corsConfigurationSource()))
+			.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+			.authorizeHttpRequests(auth -> auth
+					//Solo se toca aquí
+					.requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
+					.requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
+					.requestMatchers(HttpMethod.POST, "/auth/refresh").permitAll()
+					.requestMatchers(HttpMethod.GET, "/tareas").hasAnyRole("USER", "ADMIN")
+					.requestMatchers(HttpMethod.GET, "/tareas/*").hasAnyRole("ADMIN")
+					.anyRequest().permitAll()
+					)
+			.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+		
+		return http.build();	
+	}
+	
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        // Divide la cadena por comas y elimina espacios en blanco
+        List<String> allowedOrigins = Arrays.stream(frontendUrls.split(","))
+                                            .map(String::trim)
+                                            .toList();
+//        List<String> allowedOrigins = Arrays.asList("http://localhost:4200");
+        configuration.setAllowedOrigins(allowedOrigins);
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "Accept"));
+        configuration.setExposedHeaders(List.of("Authorization"));
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+    
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+    
+    @Bean
+    AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }    
+
+}
